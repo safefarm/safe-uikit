d1643b1de26bc5247221376eff4618ae1abd1479
musclefinance <82107987+safefarm@users.noreply.github.com>
2021-04-07 19:17:52 +0200

    Update index.cjs.js

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1877,8 +1877,8 @@ var Icon$P = function (props) {
 
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
-        React__default['default'].createElement("image", { width: "205", height: "26", href: isDark ? '/images/muscle/LogoTextNewDark.png' : '/images/muscle/LogoTextNewWhite.png' })));
+    return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 205 40" }, props),
+        React__default['default'].createElement("image", { width: "205", height: "40", href: isDark ? '/images/muscle/LogoTextNewDark.png' : '/images/muscle/LogoTextNewWhite.png' })));
 };
 
 var Icon$Q = function (props) {

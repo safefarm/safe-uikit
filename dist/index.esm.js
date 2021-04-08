56c328e6d17e13faeee3354991d412ead8ca84a6
safefarm <82107987+safefarm@users.noreply.github.com>
2021-04-08 02:04:47 +0200

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2822,20 +2822,21 @@ var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { 
-background: "#FAF9FA", // #FAF9FA
-    backgroundDisabled: "#E9EAEB", 
-    contrast: "#191326", 
-    invertedContrast: "#FFFFFF", 
-    input: "#eeeaf4", 
-    tertiary: "#EFF4F5",
-    // text: "#452A7A",  
-    text: "#2b2e4a", 
-    textDisabled: "#BDC2C4", 
-    /// textSubtle: "#8f80ba",
-    textSubtle: "#2171b0", // #903749
-    borderColor: "#E9EAEB", 
-    card: "#FFFFFF", gradients: {
-        bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+    secondary: "#fc6060", 
+    background: "#FAF9FA", 
+    backgroundDisabled: "#3c3742", 
+    contrast: "#FFFFFF", 
+    invertedContrast: "#191326", 
+    input: "#483f5a", 
+    primaryDark: "#0098A1", 
+    tertiary: "#353547", 
+    /// text: "#ffd1d1", 
+    text: "#c2c2c2",
+    textDisabled: "#666171", 
+    textSubtle: "#c2c2c2", 
+    borderColor: "#524B63", 
+    card: "#27262c", gradients: {
+        bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
 var light = {
